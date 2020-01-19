eb931b6b800f109d1454070799d87983f51112d8
Jeremy O <galaxyjay123@gmail.com>
2020-01-19 11:34:00 -0700

    added purchase view

---
+++ src/views/PurchasePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="purchase">
+
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'purchase',
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {},
+    components: {}
+  }
+</script>
+
+
+<style scoped>
+
+</style>
